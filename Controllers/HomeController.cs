f9c9e1b1a69b97027b4db74d16b316d58bda08c3
potlitel <potlitel@gmail.com>
2023-09-03 21:02:44 -0400

    Se completan las tarjetas del dashboard para las entidades Géneros y Libros.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,30 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BootstrapDashboard.Models;
+using BootstrapDashboard.Services;
 
 namespace BootstrapDashboard.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IGenerosService _generosService;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly ILibrosService _librosService;
+
+    public HomeController(ILogger<HomeController> logger, IGenerosService generosService, ILibrosService librosService)
     {
         _logger = logger;
+        _generosService = generosService;
+        _librosService = librosService;
     }
 
     public IActionResult Index()
     {
+        var countGeneros = _generosService.GetGenerosAsync().Count();
+        var countLibros = _librosService.GetLibrosAsync().Count();
+        ViewBag.Generos = countGeneros;
+        ViewBag.Libros = countLibros;
         return View();
     }
 
