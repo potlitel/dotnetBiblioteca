c3229582d2a57628b947856346e056327987893b
potlitel <potlitel@gmail.com>
2023-09-06 18:12:53 -0400

    Se muestra listado de lectores desde su correspondiente vista.

--- Controllers/LectorController.cs
+++ Controllers/LectorController.cs
@@ -2,22 +2,51 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BootstrapDashboard.Models;
 using Microsoft.AspNetCore.Authorization;
+using BootstrapDashboard.Services;
+using X.PagedList;
 
 namespace BootstrapDashboard.Controllers;
 
 public class LectorController : Controller
 {
-    // private readonly IGenerosService albumesService;
+    private readonly ILectoresService _lectoresService;
 
-    public LectorController()
+    public LectorController(ILectoresService lectoresService)
     {
-        // albumesService = service;
+        _lectoresService = lectoresService;
     }
 
-    public IActionResult Index()
+    /**
+     * Description: Index method
+     * @param {any} stringsortOrder
+     * @param {any} stringq
+     * @param {any} int?page
+     * @returns {any}
+        */
+    public async Task<IActionResult> Index(string sortOrder, string q, int? page)
     {
-        // var result = albumesService.GetGenerosAsync();
-        return View();
+        // Thread.Sleep(2000);
+        var result = await _lectoresService.GetLectoresAsync();
+        var count = await _lectoresService.GetLectoresCount();
+        if (!string.IsNullOrEmpty(q))
+        {
+            page = 1;
+            result = result.Where(s => s.Nombre.ToUpper().Contains(q.ToUpper())
+                                   || s.Apellidos.ToUpper().ToString().Contains(q.ToUpper())
+                                   || s.Email.ToUpper().ToString().Contains(q.ToUpper())
+                                   || s.Direccion.ToUpper().ToString().Contains(q.ToUpper())
+                                   || s.Telefono.ToUpper().ToString().Contains(q.ToUpper())).ToList();
+        }
+        if (count == 0 && q != string.Empty)
+            TempData["mensaje"] = "No se ha encontrado ningún lector con el criterio de búsqueda especificado.";
+        else
+        if (count == 0 && q == string.Empty)
+            TempData["mensaje"] = "No existen lectores para mostrar.";
+
+        int pageSize = 5;
+        int pageNumber = page ?? 1;
+        ViewBag.pageListModel = result.ToPagedList(pageNumber, pageSize);
+        return View(result.ToPagedList(pageNumber, pageSize));
     }
 
     [AcceptVerbs("Get", "Post")]
